0ac109414c0fb88df9bb581ec95e721a9dca659a
feizheng <1290657123@qq.com>
2016-10-21 15:39:23 +0800

    update ssh

--- index.sh
+++ index.sh
@@ -5,4 +5,5 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 alias ssh-cmsn='ssh root@58.221.61.241';
 alias ssh-zgmw='ssh root@118.178.20.114';
 alias ssh-zjdd='ssh root@121.43.149.246';
+alias ssh-yqp='ssh root@139.224.18.91';
 alias ssh-pbcopy='cat ~/.ssh/id_rsa.pub | pbcopy';
