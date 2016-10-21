4b94c596a34e086191f38d1227737d1b85aaa58e
feizheng <1290657123@qq.com>
2016-10-21 16:34:17 +0800

    update initail

--- initialize.sh
+++ initialize.sh
@@ -8,6 +8,35 @@ sudo chmod 644 ~/.ssh/known_hosts;
 sudo chmod 755 ~/.ssh;
 
 
-## ssh-copy-id 
+## ssh-copy-id
 # ssh-copy-id root@118.178.20.114
 # ssh-copy-id root@139.224.18.91
+
+
+## create ssh_pubkey for every git site:
+touch ~/.ssh/config;
+chmod 600 ~/.ssh/config;
+
+##create id_rsa
+ssh-keygen
+
+##create oschina:
+ssh-keygen -t rsa -f ~/.ssh/id_rsa.oschina.net -C "Key for git.oschina.net";
+
+##create github:
+ssh-keygen -t rsa -f ~/.ssh/id_rsa.github.com -C "Key for git.github.com";
+
+##create git.zaijiadd.com:
+ssh-keygen -t rsa -f ~/.ssh/id_rsa.zaijiadd.com -C "Key for git.zaijiadd.com";
+
+
+##add to cache:
+ssh-add ~/.ssh/id_rsa.oschina.net;
+ssh-add ~/.ssh/id_rsa.github.com;
+ssh-add ~/.ssh/id_rsa.zaijiadd.com;
+
+
+##Test git config:
+ssh -T git@git.oschina.net;
+ssh -T git@github.com;
+ssh -T git@git.zaijiadd.com;
