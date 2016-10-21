2e5dc23ee2b34fc57a65d6f0d92ea40bbabbd436
feizheng <1290657123@qq.com>
2016-10-21 15:42:57 +0800

    udpte initial

--- initialize.sh
+++ initialize.sh
@@ -6,3 +6,8 @@ sudo chmod 600 ~/.ssh/id_rsa;
 sudo chmod 600 ~/.ssh/id_rsa.pub;
 sudo chmod 644 ~/.ssh/known_hosts;
 sudo chmod 755 ~/.ssh;
+
+
+## ssh-copy-id 
+# ssh-copy-id root@118.178.20.114
+# ssh-copy-id root@139.224.18.91
