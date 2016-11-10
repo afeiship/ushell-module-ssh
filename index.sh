21041fb0a7180c619ff702f722adb588ac5176f1
feizheng <1290657123@qq.com>
2016-11-11 01:08:16 +0800

    add my aly

--- index.sh
+++ index.sh
@@ -13,6 +13,8 @@ alias ssh-cmsn='ssh root@58.221.61.241';
 alias ssh-zgmw='ssh root@118.178.20.114';
 alias ssh-zjdd='ssh root@121.43.149.246';
 alias ssh-yqp='ssh root@139.224.18.91';
+alias ssh-aly='ssh root@106.14.39.232';
+
 alias ssh-pbcopy='cat ~/.ssh/id_rsa.pub | pbcopy';
 
 ## test git:
