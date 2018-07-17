e33ffb304422993fc61a2404eca35eba6599ff19
afeiship <1290657123@qq.com>
2018-07-17 17:09:53 +0800

    issue: multiple ssh-agent bugfix

--- index.sh
+++ index.sh
@@ -4,9 +4,11 @@
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
 ## start ssh-agent:
-eval `ssh-agent -s`>/dev/null 2>/dev/null;
-ssh-add ~/.ssh/id_rsa.github.com >/dev/null 2>/dev/null;
-ssh-add ~/.ssh/id_rsa.oschina.net >/dev/null 2>/dev/null;
+if [ -z "$SSH_AUTH_SOCK" ] ; then
+    eval `ssh-agent -s`>/dev/null 2>/dev/null;
+    ssh-add ~/.ssh/id_rsa.github.com >/dev/null 2>/dev/null;
+    ssh-add ~/.ssh/id_rsa.oschina.net >/dev/null 2>/dev/null;
+fi
 
 ## base ssh address:
 alias ssh-cmsn='ssh root@58.221.61.241';
