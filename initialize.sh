3e3df637d4476b905e9426eebb466ddc5e6fd8c8
afeiship <1290657123@qq.com>
2018-11-15 12:58:00 +0800

    issue: new gitlab hosts

--- initialize.sh
+++ initialize.sh
@@ -30,14 +30,19 @@ ssh-keygen -t rsa -f ~/.ssh/id_rsa.github.com -C "Key for git.github.com";
 ##create git.zaijiadd.com:
 # ssh-keygen -t rsa -f ~/.ssh/id_rsa.zaijiadd.com -C "Key for git.zaijiadd.com";
 
+##create gitlab.finxos.com
+# ssh-keygen -t rsa -f ~/.ssh/id_rsa.gitlab.finxos.com -C "Key for gitlab.finxos.com";
+
 
 ##add to cache:
 ssh-add ~/.ssh/id_rsa.oschina.net;
 ssh-add ~/.ssh/id_rsa.github.com;
 # ssh-add ~/.ssh/id_rsa.zaijiadd.com;
+# ssh-add ~/.ssh/id_rsa.gitlab.finxos.com;
 
 
 ##Test git config:
 # ssh -T git@git.oschina.net;
 # ssh -T git@github.com;
 # ssh -T git@git.zaijiadd.com;
+# ssh -T git@gitlab.finxos.com;
